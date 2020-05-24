70d698a53c036fd6ea2221dd116b2ac449e9c001
Jorge Dutton <jorge.dutton@gmail.com>
2020-05-24 09:26:11 +0200

    Added xml configuration for ChuckNorrisQuotes Bean

--- src/main/java/com/jdutton/jokes/ui/configuration/ChuckNorrisJokesConfiguration.java
+++ src/main/java/com/jdutton/jokes/ui/configuration/ChuckNorrisJokesConfiguration.java
@@ -1,14 +1,11 @@
 package com.jdutton.jokes.ui.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckNorrisJokesConfiguration {
 
-	@Bean
+	// @Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
